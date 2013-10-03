f89e931323f63624f7c8f4e9b39c2d93e81e8897
Breyten Ernsting <bje@dds.nl>
2013-10-03 14:37:49 +0200

    Crawle program info for spotvogel.

---
+++ crawler.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import json
+
+from pprint import pprint
+from time import sleep
+
+import MySQLdb
+
+import requests
+
+def get_prids():
+    conn = MySQLdb.connect(
+        host='localhost',
+        user='spotvogel',
+        passwd='spotvogel',
+        db='spotvogel'
+    )
+    cursor = conn.cursor()
+    cursor.execute("""SELECT prid FROM Video WHERE prid IS NOT NULL""")
+    return [r[0] for r in cursor.fetchall() if re.search('^VARA_', r[0])]
+
+def main():
+    prids = get_prids()
+    videos = []
+    for prid in prids:
+        contents = requests.get(u'http://e.omroep.nl/metadata/aflevering/%s' % (prid,)).text
+        video = json.loads(contents[14:-2])
+        videos.append(video)
+        sleep(1)
+    print json.dumps(videos)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
